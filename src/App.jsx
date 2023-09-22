50c10994703f0e543c683105d92864533c4d8c76
EricHsu <hsu14253@gmail.com>
2023-09-22 22:50:18 +0800

    rm: /dailychrono

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Project from "./components/project/Project";
-import Dailychrono from "./components/dailychrono/Dailychrono";
+// import Dailychrono from "./components/dailychrono/Dailychrono";
 
 import { Routes, Route } from "react-router-dom";
 
